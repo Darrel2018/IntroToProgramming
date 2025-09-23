7af6acb0a5ef30a41ac545bf682b63766ebc8af8
Darrel <37542620+Darrel2018@users.noreply.github.com>
2025-09-23 13:06:50 +0200

    Add files via upload

---
+++ Book2/Chapter2/Chapter2.js
@@ -0,0 +1,40 @@
+alert("Coding is Fun!");
+
+// A Program to Print a Message 300 Times
+
+for(var i = 0; i < 300; i++) {
+    document.write("Coding is Fun !!!!");
+}
+
+var element = document.createElement("div");
+element.textContent = "Testing";
+document.body.append(element);
+
+// Escaping Quotation Marks
+
+for (var i = 0; i < 100; i++) { 
+    document.write ("Joe said,\"Hi!\"");
+}
+
+// Double Quotes within Single Quotes
+
+for (var i = 0; i < 100; i++) {
+  document.write('Joe said,"Hi!"');
+}
+
+for (var i = 0; i < 100; i++) {
+  document.write("Joe said,'Hi!'");
+}
+
+// Making comments
+
+// THIS IS A SINGLE LINE COMMENT
+
+/*
+This
+Is
+A
+Multi
+Line
+Comment
+*/
