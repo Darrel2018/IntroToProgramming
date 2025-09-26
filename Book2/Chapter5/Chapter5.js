54e573ecf166670eda6501abf388cc609559b0c8
Darrel <37542620+Darrel2018@users.noreply.github.com>
2025-09-26 12:40:21 +0200

    Add files via upload

---
+++ Book2/Chapter5/Chapter5.js
@@ -0,0 +1,20 @@
+// document.getElementById("aboutMe").innerHTML = "This is <em>your</em> paragraph!";
+
+var item1;
+var item2;
+var item3;
+
+function newList() {
+  item1 = prompt("Enter a new first thing: ");
+  item2 = prompt("Enter a new second thing: ");
+  item3 = prompt("Enter a new third thing: ");
+  updateList();
+}
+
+function updateList() {
+  document.getElementById("firstThing").innerHTML = item1;
+  document.getElementById("secondThing").innerHTML = item2;
+  document.getElementById("thindThing").innerHTML = item3;
+}
+
+document.getElementById("changeList").onclick = newList;
